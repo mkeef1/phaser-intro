9bcbe158c107adb270dea9cf7f1591bb3640bd49
Matt Keef <mkeef1@gmail.com>
2014-12-10 23:46:33 -0600

    init

--- public/main.js
+++ public/main.js
@@ -1,45 +1,35 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update:update});
+var game = new Phaser.Game(900, 600, Phaser.AUTO, 'capstone', { preload: preload, create: create, update:update});
 
 function preload(){
-  game.load.image('bear', '/img/8559402848_9fcd90d20b_b.jpg');
-  game.load.image('frog', '/img/demo-image1.jpg');
-  game.load.atlasJSONHash('bot', '/img/running_bot.png', '/img/running_bot.json');
+  game.load.image('quas', '/img/quas.gif');
+  game.load.spritesheet('quasrun', '/img/quasrun.png', 128, 128);
 }
 
-var s1;
+var player, cursors;
 
 function create(){
-  var bot = game.add.sprite(200, 200, 'bot');
-  bot.animations.add('run');
-  bot.animations.play('run', 15, true);
-
-  s1 = game.add.sprite(20, 30, 'bear');
-  var s2 = game.add.sprite(60, 60, 'frog');
-  s2.scale.setTo(.1);
-  s1.scale.setTo(.1);
-
-  game.physics.enable(s1, Phaser.Physics.ARCADE);
-  game.physics.enable(s2, Phaser.Physics.ARCADE);
-
-  s1.body.velocity.x = 75;
-
-  var text = "- phaser -\n move the bear! \n watch the flowers go!";
-  var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
-  var t = game.add.text(game.world.centerX-300, 0, text, style);
-
-  var tween = game.add.tween(s2);
-  tween.to({x:600}, 1000);
-  tween.start();
-
-  tween = game.add.tween(bot);
-  tween.to({y:300}, 1500);
-  tween.start();
+  player = game.add.sprite(100, 200, 'quasrun');
+  //game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.arcade.enable(player);
+  player.animations.add('left', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10, true);
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update(){
-  if(game.physics.arcade.distanceToPointer(s1, game.input.activePointer) > 8){
-    game.physics.arcade.moveToPointer(s1, 300);
+  player.body.velocity.x = 0;
+  player.animations.play('left');
+  if(cursors.left.isDown){
+    player.body.velocity.x = -150;
+    player.animations.play('left');
+  }
+  else if(cursors.right.isDown){
+    player.body.velocity.x = 150;
+    player.animations.play('left');
   }else{
-    s1.body.velocity.set(0);
+    player.animations.stop();
+    player.frame = 11;
+  }
+  if(cursors.up.isDown && player.body.touching.down){
+    player.body.velocity.y = -350;
   }
 }
